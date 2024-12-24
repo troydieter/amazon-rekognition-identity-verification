d4ffae9b109ccd9dc6581dcd37166711c59bdbcf
Troy Dieter <troydieter@gmail.com>
2024-12-23 23:24:20 -0500

    changed casing

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from idplusselfie.idplusselfie_stack import idplusselfieStack
+from idplusselfie.idplusselfie_stack import IdPlusSelfieStack
 
 
 app = cdk.App()
-idplusselfieStack(app, "idplusselfieStack",
+IdPlusSelfieStack(app, "idplusselfieStack",
                   # If you don't specify 'env', this stack will be environment-agnostic.
                   # Account/Region-dependent features and context lookups will not work,
                   # but a single synthesized template can be deployed anywhere.
--- idplusselfie/idplusselfie_stack.py
+++ idplusselfie/idplusselfie_stack.py
@@ -2,41 +2,42 @@ from aws_cdk import (
     Stack,
     aws_lambda as _lambda,
     aws_iam as iam,
-    aws_apigateway as apigateway
+    aws_apigateway as apigateway,
 )
 from constructs import Construct
 
 
-class idplusselfieStack(Stack):
+class IdPlusSelfieStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Define the lambda resource
-        ips_lambda = _lambda.Function(self, "ipsHandler",
-                                      code=_lambda.Code.from_asset('lambda'),
-                                      handler="ips-lambda.lambda_handler",
-                                      runtime=_lambda.Runtime.PYTHON_3_9,
-                                      )
+        # Define the Lambda function
+        ips_lambda = _lambda.Function(
+            self,
+            "IpsHandler",
+            code=_lambda.Code.from_asset("lambda"),
+            handler="ips_lambda.lambda_handler",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+        )
 
-        # Create an IAM policy statement for the function to request rekognition
-        ips_lambda.add_to_role_policy(iam.PolicyStatement(
-            effect=iam.Effect.ALLOW,
-            actions=[
-                'rekognition:CompareFaces',
-            ],
-            resources=[
-                '*'
-            ],
-        ))
+        # Attach an IAM policy for the Lambda function to allow Rekognition actions
+        ips_lambda.add_to_role_policy(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=["rekognition:CompareFaces"],
+                resources=["*"],  # Limit this further if possible for better security
+            )
+        )
 
-        # Define API Gateway
-        api = apigateway.LambdaRestApi(self, "ipsApi",
-                                       handler=ips_lambda,
-                                       proxy=False)
+        # Define API Gateway and associate it with the Lambda function
+        api = apigateway.LambdaRestApi(
+            self,
+            "IpsApi",
+            handler=ips_lambda,
+            proxy=False,
+        )
 
-        # Define resource
-        items = api.root.add_resource("ips")
-        items.add_method("POST") # POST /spi
-
-
+        # Define API Gateway resource and method
+        ips_resource = api.root.add_resource("ips")
+        ips_resource.add_method("POST")  # POST /ips
