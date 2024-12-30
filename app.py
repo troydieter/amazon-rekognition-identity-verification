35a4ee9871f725ee63f66288880f7fdff87a2f6b
Troy Dieter <troydieter@gmail.com>
2024-12-30 12:53:51 -0500

    added tags

--- app.py
+++ app.py
@@ -25,4 +25,6 @@ IdPlusSelfieStack(app, "idplusselfieStack",
                   # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
                   )
 
+cdk.Tags.of(app).add("project", "identity-verification")
+
 app.synth()
